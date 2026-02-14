b9362611d4907b511cba0d8ff9c98ba3b1b85b1a
DiopYahya <250817@etudiant.cegepvicto.ca>
2026-02-14 12:33:07 -0500

    SolutionSation meteo

---
+++ SolutionStationMeteo/StationMeteo.App/Program.cs
@@ -0,0 +1,17 @@
+﻿using StationMeteo;
+using StationMeteo.Metier;
+
+namespace StationMeteo.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CapteurMeteo v = new CapteurMeteo("Capteur_Nord",12,-10);
+            Console.WriteLine(v.Nom);
+            Console.WriteLine(v.Temperature);
+            Console.WriteLine(v.Humidite);
+
+        }
+    }
+}
